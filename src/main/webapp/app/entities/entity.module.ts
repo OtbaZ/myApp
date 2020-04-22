9e33f9f687fbb79a4697a49456f8d14adb50d9bb
Otba Zeramdini <otba.zeramdini@gmail.com>
2020-04-22 18:38:17 +0200

    Réalisation de la première partie du DM, Otba ZERAMDINI

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,34 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'restaurant',
+        loadChildren: () => import('./restaurant/restaurant.module').then(m => m.MyAppRestaurantModule)
+      },
+      {
+        path: 'cooperative',
+        loadChildren: () => import('./cooperative/cooperative.module').then(m => m.MyAppCooperativeModule)
+      },
+      {
+        path: 'basket',
+        loadChildren: () => import('./basket/basket.module').then(m => m.MyAppBasketModule)
+      },
+      {
+        path: 'product',
+        loadChildren: () => import('./product/product.module').then(m => m.MyAppProductModule)
+      },
+      {
+        path: 'payment',
+        loadChildren: () => import('./payment/payment.module').then(m => m.MyAppPaymentModule)
+      },
+      {
+        path: 'course',
+        loadChildren: () => import('./course/course.module').then(m => m.MyAppCourseModule)
+      },
+      {
+        path: 'order',
+        loadChildren: () => import('./order/order.module').then(m => m.MyAppOrderModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
